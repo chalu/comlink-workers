9c328d25396653b0036953a0c230fc57ec626b44
Charles Odili <chaluwa@gmail.com>
2019-12-06 02:03:41 +0100

    analyze text on ui-thread

--- src/utils.js
+++ src/utils.js
@@ -18,9 +18,28 @@ const getNextTime = (uiElement) => {
 export const showTime = (uiElement) => {
     return () => {
         if(!uiElement) return;
-        
+
         const { hours, minutes, seconds } = getTimeFromCurrentDate();
         uiElement.textContent = `${hours}:${minutes}:${seconds}`;
         getNextTime(uiElement);
     };
+};
+
+export const getFPSMonitor = () => {
+    const times = [];
+    const monitor = (uiElement) => {
+        requestAnimationFrame(() => {
+            const now = performance.now();
+            while (times.length > 0 && times[0] <= now - 1000) {
+                times.shift();
+            }
+            times.push(now);
+
+            // const frames = times.length > 60 ? 60 : times.length;
+            uiElement.textContent = `${times.length} FPS`;
+
+            monitor(uiElement);
+        });
+    };
+    return monitor;
 };
