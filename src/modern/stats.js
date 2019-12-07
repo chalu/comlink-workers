24e74839f4d14bc4aab5d6125d72e7804e26e498
Charles Odili <chaluwa@gmail.com>
2019-12-08 00:54:07 +0100

    lint and format codebase

--- src/modern/stats.js
+++ src/modern/stats.js
@@ -1,98 +1,100 @@
 importScripts("https://unpkg.com/comlink/dist/umd/comlink.js");
 
 const pipe = (...fns) => {
-    return (data) => {
-        return fns.reduce((payload, fn) => fn(payload), data);
-    };
+  return data => {
+    return fns.reduce((payload, fn) => fn(payload), data);
+  };
 };
 
-const busy = ({maxWait = 10000} = {}) => new Promise(resolve => {
-    const delay = Math.round( Math.random() * maxWait );
+const busy = ({ maxWait = 10000 } = {}) =>
+  new Promise(resolve => {
+    const delay = Math.round(Math.random() * maxWait);
     setTimeout(resolve, delay);
-});
+  });
 
-const massageData = (payload) => {
-    payload.text = (payload.text || '').trim();
-    payload.stats = {};
-    return payload;
+const massageData = payload => {
+  payload.text = (payload.text || "").trim();
+  payload.stats = {};
+  return payload;
 };
 
-const countChars = (payload) => {
-    const { text } = payload;
-    payload.stats.chars = text.length;
-    return payload;
+const countChars = payload => {
+  const { text } = payload;
+  payload.stats.chars = text.length;
+  return payload;
 };
 
-const countWords = (payload) => {
-    const { text } = payload;
-    payload.stats.words = text.split(/\s+/g).length;
-    return payload;
+const countWords = payload => {
+  const { text } = payload;
+  payload.stats.words = text.split(/\s+/g).length;
+  return payload;
 };
 
-const countLines = (payload) => {
-    const { text } = payload;
-    payload.stats.lines = text.split(/\n/g).length;
-    return payload;
+const countLines = payload => {
+  const { text } = payload;
+  payload.stats.lines = text.split(/\n/g).length;
+  return payload;
 };
 
-const findMostUsedWord = (payload) => {
-    const { text } = payload;
-    const wordMap = text.split(/\s+/g)
-        .reduce((map, word) => {
-            if (word.trim().length >= 2) {
-                word = word.replace(/\W/g, '').toLowerCase();
-                map[word] = (map[word] || 0) + 1;
-            }
-            return map;
-        }, {});
+const findMostUsedWord = payload => {
+  const { text } = payload;
+  const wordMap = text.split(/\s+/g).reduce((map, word) => {
+    if (word.trim().length >= 2) {
+      word = word.replace(/\W/g, "").toLowerCase();
+      map[word] = (map[word] || 0) + 1;
+    }
+    return map;
+  }, {});
 
-    const sorted = Object.entries(wordMap).sort((a, b) => b[1] - a[1]);
-    payload.stats.mostUsed = sorted[0];
+  const [mostUsed] = Object.entries(wordMap).sort((a, b) => b[1] - a[1]);
+  payload.stats.mostUsed = mostUsed;
 
-    return payload;
+  return payload;
 };
 
 // adapted from https://gist.github.com/sqren/5083d73f184acae0c5b7
-const clogByChars = (payload) => {
-    let result = 0, num = payload.stats.chars;
-    for (let i = Math.pow(num, 7); i >= 0; i--) {
-        result += Math.atan(i) * Math.tan(i);
-    };
-    return payload;
-}
+const clogCPUByCharsLen = payload => {
+  /* eslint-disable no-restricted-properties */
+  /* eslint-disable no-unused-expressions */
+  const num = payload.stats.chars;
+  for (let i = Math.pow(num, 7); i >= 0; i -= 1) {
+    Math.atan(i) * Math.tan(i);
+  }
+  return payload;
+};
 
 const analyze = pipe(
-    massageData,
-    countChars,
-    countWords,
-    countLines,
-    findMostUsedWord,
-    clogByChars
+  massageData,
+  countChars,
+  countWords,
+  countLines,
+  findMostUsedWord,
+  clogCPUByCharsLen
 );
 
-const checkGrammer = async (text) => {
-    await busy();   // e.g calling a remote grammer checker API
-    return {
-        grammerErrors: 0,
-        spellingErrors: 0
-    }
+const checkGrammer = async () => {
+  await busy(); // e.g calling a remote grammer checker API
+  return {
+    grammerErrors: 0,
+    spellingErrors: 0
+  };
 };
 
 // externalised "API" object
 const Analyzer = {
-    analyzeText(text) {
-        return analyze({ text });
-    },
+  analyzeText(text) {
+    return analyze({ text });
+  },
 
-    async analyzeGrammer(text, callback) {
-        // call async spelling and grammer checker
-        // then send the results back to the main thread
-        // by calling the callback function with it
-        // console.log('calling grammer checker');
-        const status = await checkGrammer(text);
-        callback({status});
-    }
+  async analyzeGrammer(text, callback) {
+    // call async spelling and grammer checker
+    // then send the results back to the main thread
+    // by calling the callback function with it
+    // console.log('calling grammer checker');
+    const status = await checkGrammer(text);
+    callback({ status });
+  }
 };
 
 // expose the "API"
-Comlink.expose(Analyzer, self);
+Comlink.expose(Analyzer);
--- src/utils.js
+++ src/utils.js
@@ -1,46 +1,48 @@
 export const getTimeFromCurrentDate = () => {
-    const now = new Date();
+  const now = new Date();
 
-    const hours = now.getHours();
-    const minutes = now.getMinutes();
-    const seconds = now.getSeconds();
-    const milliseconds = now.getMilliseconds();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
+  const milliseconds = now.getMilliseconds();
 
-    return {hours, minutes, seconds, milliseconds};
+  return { hours, minutes, seconds, milliseconds };
 };
 
-const getNextTime = (uiElement) => {
-    setTimeout(() => {
-        requestAnimationFrame(showTime(uiElement));
-    }, 1000);
+const getNextTime = uiElement => {
+  setTimeout(() => {
+    requestAnimationFrame(showTime(uiElement));
+  }, 1000);
 };
 
-export const showTime = (uiElement) => {
-    return () => {
-        if(!uiElement) return;
+export const showTime = uiElement => {
+  return () => {
+    const ui = uiElement;
+    if (!ui) return;
 
-        const { hours, minutes, seconds } = getTimeFromCurrentDate();
-        uiElement.textContent = `${hours}:${minutes}:${seconds}`;
-        getNextTime(uiElement);
-    };
+    const { hours, minutes, seconds } = getTimeFromCurrentDate();
+    ui.textContent = `${hours}:${minutes}:${seconds}`;
+    getNextTime(uiElement);
+  };
 };
 
 // adapted from https://www.growingwiththeweb.com/2017/12/fast-simple-js-fps-counter.html
 export const getFPSMonitor = () => {
-    const times = [];
-    const monitor = (uiElement) => {
-        requestAnimationFrame(() => {
-            const now = performance.now();
-            while (times.length > 0 && times[0] <= now - 1000) {
-                times.shift();
-            }
-            times.push(now);
+  const times = [];
+  const monitor = uiElement => {
+    const ui = uiElement;
+    requestAnimationFrame(() => {
+      const now = performance.now();
+      while (times.length > 0 && times[0] <= now - 1000) {
+        times.shift();
+      }
+      times.push(now);
 
-            // const frames = times.length > 60 ? 60 : times.length;
-            uiElement.textContent = `${times.length} FPS`;
+      // const frames = times.length > 60 ? 60 : times.length;
+      ui.textContent = `${times.length} FPS`;
 
-            monitor(uiElement);
-        });
-    };
-    return monitor;
+      monitor(ui);
+    });
+  };
+  return monitor;
 };
